698bee2af1d52e1f54d4d661412311e41e13162e
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-06-03 17:05:56 +0200

    Revert "debug cli"
    This reverts commit c748c9e732e96d22bb6ac45fe8b9374879e25b44.

--- pdeo/cli.py
+++ pdeo/cli.py
@@ -18,9 +18,6 @@ Options:
 Trakt options:
     -t TOKEN, --token TOKEN             OAuth token.
 
-Debug options:
-    --all                               Display all found torrents instead of one.
-
 """
 
 # TODO: single movie search
@@ -43,13 +40,9 @@ def __main__():
     else:
         print('trakt.')
         p = Core()
-        torrents = p.check('logan', 2017)
         print('FOUND:')
-        if args['--all']:
-            for i in torrents:
-                print(i)
-        else:
-            print(torrents[0])
+        for i in p.check('logan', 2017):
+            print(i)
     # if args['add']:
     #     db = database()
     #     if not db.add(tmdb_id=args['<tmdb_id>']):
